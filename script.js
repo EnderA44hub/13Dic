994586fa3b7175abd915dd77eaddbaad1a877fe9
EnderA44hub <endera2895@gmail.com>
2025-12-12 08:59:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // CAMBIA LA FECHA AQUÍ (formato: "2025-12-13 00:00:00")
-const FECHA_REVELAR = new Date("2025-12-13 00:00:00").getTime();
+const FECHA_REVELAR = new Date("2025-12-14 00:00:00").getTime();
 
 const countdownEl = document.getElementById('countdown');
 const letterEl = document.getElementById('letter');
